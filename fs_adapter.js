3cdb6a9cad1ac6fd869711bb15a43440abad9bbf
Bedz01 <10061448+Bedz01@users.noreply.github.com>
2026-05-26 16:04:04 +1200

    fix: resolve relative file paths from module directory

--- fs_adapter.js
+++ fs_adapter.js
@@ -4,22 +4,29 @@
  * If you ever switch from Deno to Node.js, you only need to change this file.
  */
 
+function resolvePath(filePath) {
+  if (/^(?:[a-zA-Z]:[\\/]|\\|\/)/.test(filePath)) {
+    return filePath;
+  }
+  return `${import.meta.dirname}/${filePath}`;
+}
+
 export async function readTextFile(filePath) {
   // Deno implementation:
-  return await Deno.readTextFile(filePath);
+  return await Deno.readTextFile(resolvePath(filePath));
   
   // Node.js implementation:
   // const fs = await import("fs/promises");
-  // return await fs.readFile(filePath, "utf-8");
+  // return await fs.readFile(resolvePath(filePath), "utf-8");
 }
 
 export async function writeTextFile(filePath, content) {
   // Deno implementation:
-  await Deno.writeTextFile(filePath, content);
+  await Deno.writeTextFile(resolvePath(filePath), content);
   
   // Node.js implementation:
   // const fs = await import("fs/promises");
-  // await fs.writeFile(filePath, content, "utf-8");
+  // await fs.writeFile(resolvePath(filePath), content, "utf-8");
 }
 
 export function getEnv(key) {
